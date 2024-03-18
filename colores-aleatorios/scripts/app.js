b3dceb5abe1e1cb84ce56a7601c96240101f7d03
JuanAlbertoCasique <130008080+Juan-casique@users.noreply.github.com>
2024-03-18 04:04:50 +0100

    Add files via upload

---
+++ colores-aleatorios/scripts/app.js
@@ -0,0 +1,25 @@
+// Seleccionar elementos del DOM
+
+const boton = document.querySelector('button');
+const color = document.getElementById('color');
+
+function generarColorHexAleatorio() {
+  let digitos = '0123456789ABCDEF';
+  let colorHex = '#';
+
+
+for (let i = 0; i < 6; i++) {
+  let indiceAleatorio = Math.floor(Math.random() * 16);
+  colorHex += digitos[indiceAleatorio];
+}
+
+return colorHex;
+};
+
+boton.addEventListener('click', function() {
+  let colorAleatorio = generarColorHexAleatorio();
+  // Actualizar Texto
+  color.textContent = colorAleatorio;
+  // Actualizar color de fondo
+  document.body.style.backgroundColor = colorAleatorio;
+})
